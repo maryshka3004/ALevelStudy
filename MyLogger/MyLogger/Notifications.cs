17b4daafb44701ba94025a193a2bb5b3e8b8ecd3
Maryna Deineko <almakaktys@gmail.com>
2023-11-12 01:23:23 +0200

    Logger commit

---
+++ MyLogger/MyLogger/Notifications.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLogger
+{
+    internal class Notifications
+    {
+        private readonly Actions _actions;
+
+        public Notifications(Actions actions)
+        {
+            _actions = actions;
+        }
+
+        public void Notify(NotifyType type, string message, string to)
+        {
+            _actions.Logging(Result.Info, $"Notification was for type {type}");
+        }
+    }
+}
+}
