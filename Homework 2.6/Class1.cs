1b082fdf8340ce0c045f9aefcdf35e03e925262f
Maryna Deineko <almakaktys@gmail.com>
2023-11-14 16:13:55 +0200

    Commit hw 2.6

---
+++ Homework 2.6/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class extensionmethodclass
+{
+	public static bool extensionmethodname(this int str, int val)
+	{
+		return str>val;
+	}
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int stri = 565;
+		bool z = stri.extensionmethodname(200);
+		Console.WriteLine("theresult {0},z");
+		Console.ReadLine();
+	}
+}
---
+++ Homework 2.6/Homework 2.6/Device.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2._6
+{
+
+    class DeviceComparer : IComparer<Device>
+    {
+        public int Compare(Device? d1, Device? d2)
+        {
+            if (d1 is null || d2 is null)
+                throw new ArgumentException("Incorrect");
+            return d1.Consumption - d2.Consumption;
+        }
+    }
+    public abstract class Device
+    {
+        public string DeviceName { get; set; }
+        public int Consumption { get; set; }
+        public Device (string deviceName, int consumption)
+        {
+
+            DeviceName = deviceName;
+            Consumption = consumption;
+        }
+        public abstract void PlugIn();
+    }
+}
